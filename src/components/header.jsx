398057b3bc78fde4a72a97ace1df377614311241
Jakevernice <jakevernice@github.com>
2025-06-23 17:11:50 +0000

    Created Header component and MusicPlayer component

---
+++ src/components/header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+ return (
+    <header
+      style={{
+        color: 'white',
+        fontSize: '3em', // Adjust size as needed
+        fontFamily: 'cursive', // Or another cursive-like font
+        textAlign: 'center',
+        padding: '20px 0', // Adjust padding as needed
+      }}
+    >JustDoIt</header>
+ );
+};
+
+export default Header;
