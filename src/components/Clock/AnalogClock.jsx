c21b26905e6764ddfea912d2c5647eae1db78690
Rohan <rohanav27@gmail.com>
2025-06-25 23:07:29 +0530

    adjusted the code of analog clock(should look ideal now)

--- src/components/Clock/AnalogClock.jsx
+++ src/components/Clock/AnalogClock.jsx
@@ -37,50 +37,54 @@ const AnalogClock = () => {
 
   return (
     <div className="text-white p-4 rounded-lg flex flex-col items-center justify-center">
-      <h2 className="text-xl font-bold mb-2">Analog Clock</h2>
-      <div className="relative w-40 h-40">
-        {/* Clock Face */}
-        <div className="w-full h-full rounded-full border-2 border-white border-opacity-30"></div>
-        
-        {/* Numbers */}
-        {[...Array(12)].map((_, i) => (
-          <div 
-            key={i}
-            className="absolute w-full h-full"
-            style={{ transform: `rotate(${(i + 1) * 30}deg)` }}
-          >
-            <span 
-              className="absolute top-1 left-1/2 -translate-x-1/2 text-xs"
-              style={{ transform: `rotate(-${(i + 1) * 30}deg)` }}
+      <h2 className="text-2xl font-bold mb-4">Analog Clock (IST)</h2>
+
+      <div className="relative w-64 h-64 rounded-full border-4 border-white bg-gray-900 shadow-md">
+        {/* Clock Numbers */}
+        {[...Array(12)].map((_, i) => {
+          const angle = (i + 1) * 30;
+          return (
+            <div
+              key={i}
+              className="absolute top-1/2 left-1/2"
+              style={{
+                transform: `
+                  rotate(${angle}deg)
+                  translate(0, -110px)
+                  rotate(-${angle}deg)
+                `,
+                transformOrigin: "center",
+              }}
             >
-              {i + 1}
-            </span>
-          </div>
-        ))}
+              <span className="text-white text-sm font-semibold">{i + 1}</span>
+            </div>
+          );
+        })}
 
         {/* Hour Hand */}
         <div
           ref={hourRef}
-          className="absolute w-1 h-12 bg-white top-1/2 left-1/2 origin-bottom"
-          style={{ transform: 'translate(-50%, -100%) rotate(0deg)' }}
+          className="absolute w-2 h-20 bg-white top-1/2 left-1/2 origin-bottom transform -translate-x-1/2 -translate-y-full transition-transform duration-300 ease-in-out"
         ></div>
+
         {/* Minute Hand */}
         <div
           ref={minuteRef}
-          className="absolute w-0.5 h-16 bg-white top-1/2 left-1/2 origin-bottom"
-          style={{ transform: 'translate(-50%, -100%) rotate(0deg)' }}
+          className="absolute w-[3px] h-28 bg-white top-1/2 left-1/2 origin-bottom transform -translate-x-1/2 -translate-y-full transition-transform duration-300 ease-in-out"
         ></div>
+
         {/* Second Hand */}
         <div
           ref={secondRef}
-          className="absolute w-px h-20 bg-red-500 top-1/2 left-1/2 origin-bottom"
-          style={{ transform: 'translate(-50%, -100%) rotate(0deg)' }}
+          className="absolute w-px h-32 bg-red-500 top-1/2 left-1/2 origin-bottom transform -translate-x-1/2 -translate-y-full transition-transform duration-100 ease-linear"
         ></div>
+
         {/* Center Circle */}
-        <div className="absolute w-2 h-2 bg-white rounded-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
+        <div className="absolute w-4 h-4 bg-white rounded-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10"></div>
       </div>
     </div>
   );
 };
 
 export default AnalogClock;
+
